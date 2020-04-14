a908cb683a4ba4f5e073336d5ef4ca3ada889317
Alexandr Grebenkin <grebyonkin.a91@gmail.com>
2020-04-14 23:34:15 +0300

    Домашнее задание №3 Добавил уведомление и логгирование о вызове методов жизненного цикла

--- app/src/main/java/com/github/alexandrgrebenkin/weatherapp/MainActivity.java
+++ app/src/main/java/com/github/alexandrgrebenkin/weatherapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.github.alexandrgrebenkin.weatherapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -21,6 +23,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null){
+            showInfo("On Create: New Activity");
+        } else {
+            showInfo("On Create: Old Activity");
+        }
 
         initialize();
         startCitiesClickListener();
@@ -51,4 +59,44 @@ public class MainActivity extends Activity {
         });
     }
 
+    private void showInfo(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        Log.d("LIFECYCLE_LOG", message);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showInfo("On Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showInfo("On Resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        showInfo("On Pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        showInfo("On Stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        showInfo("On Restart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        showInfo("On Destroy");
+    }
 }
