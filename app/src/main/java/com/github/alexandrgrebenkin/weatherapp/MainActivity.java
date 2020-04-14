633cb938ff9ee277d7e4b2d8b9d47553e3454b7c
Alexandr Grebenkin <grebyonkin.a91@gmail.com>
2020-04-15 00:26:55 +0300

    Домашнее задание №3 Добавил сохранение данных главного экрана и к нопку для их обновления.

--- app/src/main/java/com/github/alexandrgrebenkin/weatherapp/MainActivity.java
+++ app/src/main/java/com/github/alexandrgrebenkin/weatherapp/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +20,13 @@ public class MainActivity extends Activity {
 
     private ImageButton cities;
     private ImageButton settings;
+
+    private Button refresh;
+
+    private final String CITY = "CITY";
+    private final String TEMPERATURE = "TEMPERATURE";
+    private final String WIND = "WIND";
+    private final String PRESSURE = "PRESSURE";
 
 
     @Override
@@ -33,7 +43,7 @@ public class MainActivity extends Activity {
         initialize();
         startCitiesClickListener();
         startSettingsClickListener();
-
+        startRefreshClickListener();
     }
 
     private void initialize() {
@@ -43,6 +53,7 @@ public class MainActivity extends Activity {
         pressureValue = findViewById(R.id.activity_main__tv_pressure_value);
         cities = findViewById(R.id.activity_main__ib_cities);
         settings = findViewById(R.id.activity_main__ib_settings);
+        refresh = findViewById(R.id.activity_main__btn_refresh);
     }
 
     private void startCitiesClickListener() {
@@ -59,7 +70,39 @@ public class MainActivity extends Activity {
         });
     }
 
-    private void showInfo(String message){
+    /*
+    * Временная мера для генерации данных
+    * */
+    private void startRefreshClickListener() {
+        refresh.setOnClickListener((v) -> {
+            String temperature = Integer.valueOf((int) (Math.random()*60-30)).toString();
+            String wind = Integer.valueOf((int) (Math.random()*30)).toString();
+            String pressure = Integer.valueOf((int) (Math.random()*100+700)).toString();
+            temperatureValue.setText(temperature);
+            windValue.setText(wind);
+            pressureValue.setText(pressure);
+        });
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        city.setText(savedInstanceState.getString(CITY));
+        temperatureValue.setText(savedInstanceState.getString(TEMPERATURE));
+        windValue.setText(savedInstanceState.getString(WIND));
+        pressureValue.setText(savedInstanceState.getString(PRESSURE));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(CITY, city.getText().toString());
+        outState.putString(TEMPERATURE, temperatureValue.getText().toString());
+        outState.putString(WIND, windValue.getText().toString());
+        outState.putString(PRESSURE, pressureValue.getText().toString());
+    }
+
+    private void showInfo(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
         Log.d("LIFECYCLE_LOG", message);
     }
--- app/src/main/java/com/github/alexandrgrebenkin/weatherapp/SettingsActivity.java
+++ app/src/main/java/com/github/alexandrgrebenkin/weatherapp/SettingsActivity.java
@@ -2,6 +2,9 @@ package com.github.alexandrgrebenkin.weatherapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
 
 public class SettingsActivity extends Activity {
 
